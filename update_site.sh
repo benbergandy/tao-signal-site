dce674f657af3a15972796ef80c23a4512d0a6b2
Ben Bergandy <benbergandy@gmail.com>
2026-03-22 11:34:49 -0400

    daily update 2026-03-22

---
+++ update_site.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /Users/benbergandy/Documents/tao-signal-site
+
+cp /Users/benbergandy/Documents/tao-dashboard/data/chain_data.json data/
+cp /Users/benbergandy/Documents/tao-dashboard/data/combined_scores.json data/
+cp /Users/benbergandy/Documents/tao-dashboard/data/quality_scores.json data/
+cp /Users/benbergandy/Documents/tao-dashboard/data/performance_log.json data/
+cp /Users/benbergandy/Documents/tao-dashboard/data/subnets.json data/
+
+TODAY=$(date +%Y-%m-%d)
+cp /Users/benbergandy/Documents/tao-dashboard/data/chain_history/${TODAY}.json data/chain_history/
+
+git add .
+git commit -m "daily update ${TODAY}" --allow-empty
+git push
